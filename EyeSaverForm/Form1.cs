1bd034f212c8e9fb2b2b41f351a7b6f805532d43
Matt Clarkson <mattclarkson.web@gmail.com>
2016-12-10 15:43:55 +0000

    Added break timer

--- EyeSaverForm/Form1.cs
+++ EyeSaverForm/Form1.cs
@@ -11,50 +11,41 @@ namespace EyeSaverForm
         {
             InitializeComponent();
         }
-        
+
         private void startWorkButton_Click(object sender, EventArgs e)
         {
             SetupAndStartWorkIntervalTimer(WorkIntervalTextBox.Text);
-            //WindowState = FormWindowState.Minimized;
             ShowWorkStarted();
         }
 
+        //timer initiation for countdown display
         private void ShowWorkStarted()
         {
-
             timer1.Enabled = true;
-            DateTime startTime  = DateTime.Now;
+            DateTime startTime = DateTime.Now;
             timer1.Tick +=
                 (obj, args) =>
-                    label3.Text = (TimeSpan.FromMinutes(_intervalTime/1000) - (DateTime.Now - startTime)).ToString("hh\\:mm\\:ss");
+                    label3.Text = (TimeSpan.FromMinutes(_intervalTime / 1000) - (DateTime.Now - startTime)).ToString("hh\\:mm\\:ss");
             WorkPeriodBox.Visible = false;
             WorkStartedBox.Visible = true;
-
         }
+
+        // iniates timer till next break
         private void SetupAndStartWorkIntervalTimer(string intervalPeriodMins)
         {
-            
-
-            intervalTimer.Tick += WorkBreakTimeEvent;
-            intervalTimer.Enabled = true;
+            timeTillBreakTimer.Tick += WorkBreakTimeEvent;
+            timeTillBreakTimer.Enabled = true;
             _intervalTime = GetMilliseconds(intervalPeriodMins);
-            intervalTimer.Interval = _intervalTime;
-            intervalTimer.Start();
+            timeTillBreakTimer.Interval = _intervalTime;
+            timeTillBreakTimer.Start();
         }
 
-        //shared 
-        private int GetMilliseconds(string intervalPeriodMins)
-        {
-            int intervalInMilliseconds = int.Parse(intervalPeriodMins)*1000;
-            return intervalInMilliseconds;
-        }
-
+        // break time! 
         private void WorkBreakTimeEvent(object sender, EventArgs eventInfo)
         {
-                ShowStartBreakBox();
-                HideWorkStartedBox();
-                intervalTimer.Stop();
-
+            ShowStartBreakBox();
+            HideWorkStartedBox();
+            timeTillBreakTimer.Stop();
         }
 
         private void ShowStartBreakBox()
@@ -67,29 +58,21 @@ namespace EyeSaverForm
             WorkStartedBox.Visible = false;
         }
 
+        // star break - Yes
         private void StartBreakYes_Click(object sender, EventArgs e)
         {
             BreakPeriodBox.Visible = true;
             StartBreakBox.Visible = false;
-
         }
 
+        // set break-time duration up
         private void StartBreakButton_Click(object sender, EventArgs e)
         {
             SetupAndStartIntervalTimerForBreak(BreakIntervalTextBox.Text);
-            //WindowState = FormWindowState.Minimized;
             ShowBreakStarted();
         }
 
-        private void SetupAndStartIntervalTimerForBreak(string breakIntervalPeriodMins)
-        {
-            intervalTimer.Tick += BreakTimeEvent;
-            intervalTimer.Enabled = true;
-            _intervalTime = GetMilliseconds(breakIntervalPeriodMins);
-            intervalTimer.Interval = _intervalTime;
-            intervalTimer.Start();
-        }
-
+        //timer initiation for countdown display
         private void ShowBreakStarted()
         {
             timer1.Enabled = true;
@@ -101,11 +84,22 @@ namespace EyeSaverForm
             BreakStartedBox.Visible = true;
         }
 
+        // initiates timer till work starts again 
+        private void SetupAndStartIntervalTimerForBreak(string breakIntervalPeriodMins)
+        {
+            timeTillWorkTimer.Tick += BreakTimeEvent;
+            timeTillWorkTimer.Enabled = true;
+            _intervalTime = GetMilliseconds(breakIntervalPeriodMins);
+            timeTillWorkTimer.Interval = _intervalTime;
+            timeTillWorkTimer.Start();
+        }
+
+        // break time is over event
         private void BreakTimeEvent(object sender, EventArgs e)
         {
             HideBreakStartedBox();
             ShowStartWorkBox();
-            intervalTimer.Stop();
+            timeTillWorkTimer.Stop();
         }
 
         private void HideBreakStartedBox()
@@ -117,11 +111,19 @@ namespace EyeSaverForm
             StartWorkBox.Visible = true;
         }
 
+        // start work again set up
         private void StartWorkYes_Click(object sender, EventArgs e)
         {
             WorkPeriodBox.Visible = true;
             StartWorkBox.Visible = false;
-        } 
+        }
+
+        //shared 
+        private int GetMilliseconds(string intervalPeriodMins)
+        {
+            int intervalInMilliseconds = int.Parse(intervalPeriodMins) * 1000;
+            return intervalInMilliseconds;
+        }
 
     }
 }
